96ad750cfe35377027e240459214374b8c0973d1
Yan <100032599+whykay-01@users.noreply.github.com>
2023-05-30 19:20:21 +0700

    wip: added new functions

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+
 def scrape_emails(url):
     """
     Args: URL of the webpage to scrape
@@ -11,10 +12,10 @@ def scrape_emails(url):
     response = requests.get(url)
 
     # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, "lxml")
 
     # Find all email addresses using regular expressions
-    email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    email_regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
     emails = re.findall(email_regex, soup.get_text())
     emails = list(dict.fromkeys(emails))
 
@@ -23,29 +24,63 @@ def scrape_emails(url):
     else:
         return emails
 
-def main():
-    url = input("Enter the URL to scrape: ")
-    # check if the url is valid
+
+def check_connection():
+    """
+    checks if the user is connected to the internet
+    Args: None
+    Returns: True if connected, False otherwise
+    """
+    try:
+        requests.get("https://google.com")
+        return True
+    except:
+        print("Failed! Please check your internet connection")
+        return False
+
+
+def validity_check(url):
+    """
+    checks the validity of the connection
+    Args: URL of the webpage to scrape
+    Returns: URL if valid, None otherwise
+    """
+    # validity
     if not url.startswith("http"):
         url = "https://" + url
-    # check if the url exists
+    # existense
     try:
         req = requests.get(url)
         print(req)
     except:
         print("Invalid URL")
-        return
-    emails = scrape_emails(url)
-    # Saving the emails to a file
-    if emails:
-        file_name = input("Enter the name of the file to save the results to: ")
-        with open(f"results/{file_name}.txt", "w") as f:
-            for email in emails:
-                f.write(email + "\n")
-        print(f"Emails saved to results/{file_name}.txt")
-    # if the response is None, print an error message
+        return None
+
+    return url
+
+
+def main():
+    flag = check_connection()
+
+    if flag:
+        url = input("Enter the URL to scrape: ")
+        validity_check(url)
+
+        emails = scrape_emails(url)
+        # Saving the emails to a file
+        if emails:
+            file_name = input("Enter the name of the file to save the results to: ")
+            with open(f"results/{file_name}.txt", "w") as f:
+                for email in emails:
+                    f.write(email + "\n")
+            print(f"Emails saved to results/{file_name}.txt")
+        # if the response is None, print an error message
+        else:
+            print("No emails found...")
+
     else:
-        print("No emails found...")
+        return None
+
 
 if __name__ == "__main__":
     main()
